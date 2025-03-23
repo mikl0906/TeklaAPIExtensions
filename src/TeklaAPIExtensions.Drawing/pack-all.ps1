e5ea6ed9f6089ee66db7cc5965c902a2eb442eb3
Mikhail Koshelev <koshelev.mikhail@outlook.com>
2025-03-23 18:12:10 +0200

    Update project files to support conditional target frameworks based on Tekla version and bump versions to 0.0.5 and 0.0.8; add packaging scripts for all Tekla versions.

---
+++ src/TeklaAPIExtensions.Drawing/pack-all.ps1
@@ -0,0 +1,10 @@
+$projectFile = "TeklaAPIExtensions.Drawing.csproj"
+[xml]$project = Get-Content $projectFile
+$baseVersion = $project.Project.PropertyGroup.Version
+
+$tsVersions = @("2022", "2023", "2024", "2025")
+
+foreach ($tsVersion in $tsVersions) {
+    $packageVersion = "$baseVersion-ts$tsVersion"
+    dotnet pack --configuration Release -p:TeklaVersion=$tsVersion -p:PackageVersion=$packageVersion
+}
